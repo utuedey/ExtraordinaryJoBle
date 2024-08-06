030c25965d146d967c979ef2ea83d8aa2fa06261
utuedey <utuedeyejosph@gmail.com>
2024-08-06 15:17:46 +0100

    Add logger

--- backend/app.js
+++ backend/app.js
@@ -3,17 +3,24 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const mongoose = require('mongoose');
+const pinoHttp = require('pino-http');
+const pinoLogger = require('./logger')
+const path = require('path')
+const connectToDatabase = require('./models/db');
 
 const authRoutes = require('./routes/authRoutes');
 const reminderRoutes = require('./routes/reminders');
 const scheduleRoutes = require('./routes/schedules');
 const encouragementRoutes = require('./routes/encouragements');
+const logger = require('./logger')
 
 const app = express();
 
+// Middlewares
 app.use(bodyParser.json());
-app.use(cors());
+app.use('*', cors());
+app.use(pinoHttp({ logger }))
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 app.use('/api/v1/auth', authRoutes);
@@ -25,15 +32,13 @@ app.get('/', (req, res) => {
     res.send('Welcome to Extraordinary JoBle API');
 });
 
-const mongoDBURL = process.env.MONGODB_URL;
 const PORT = process.env.PORT || 5000;
 
 // connect to database
-mongoose
-.connect(mongoDBURL)
+connectToDatabase()
 .then(() => {
     // the express server should run only when the database is connected
-    console.log('App connected to database');
+    pinoLogger.info('App connected to database');
     app.listen(PORT, function server() {
         console.log(`App is listening on port ${PORT}`)
     });
