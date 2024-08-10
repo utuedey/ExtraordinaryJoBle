f7faa552e91d84b8ed595c7682819dfe8379d4f2
utuedey <utuedeyejosph@gmail.com>
2024-08-10 22:35:13 +0100

    Add authMiddleware

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -1,16 +1,22 @@
 require("dotenv").config();
-const jwt = require('jsonwebtokens');
+const jwt = require('jsonwebtoken');
+const blacklist = new Set();
 
 function authenticateToken(req, res, next) {
     const authHeader = req.headers.authorization;
     const token = authHeader && authHeader.split(' ')[1];
 
     if (!token) {
-        return res.sendStatus(401);
+        return res.status(401).send({ message: "Access Denied" });
     }
+
+    if (blacklist.has(token)) {
+        return res.status(403).send({ message: 'Token has been invalidated'});
+    }
+
     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
         if (err) {
-            return res.sendStatus(403)
+            return res.status(403).send({ message: "Invalid Token"})
         }
 
         req.user = user;
@@ -18,4 +24,8 @@ function authenticateToken(req, res, next) {
     });
 }
 
-module.exports = authenticateToken;
+const invalidateToken = (token) => {
+    blacklist.add(token);
+}
+
+module.exports = {authenticateToken, invalidateToken};
