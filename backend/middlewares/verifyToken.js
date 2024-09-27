0c0a715d635a8467969119aa6aebd79955f8e96b
utuedey <utuedeyejosph@gmail.com>
2024-09-27 20:21:43 +0100

    Add verifyToken Middleware

---
+++ backend/middlewares/verifyToken.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+const jwt = require('jsonwebtoken');
+
+
+exports.verifyToken = (req, res, next) => {
+    const token = req.cookies.token;
+
+    if (!token) {
+        return res.status(401).send({
+             success: false,
+             message: "Unauthorized - no token provided"
+            });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRECT)
+
+        if (!decoded) {
+            return res.status(401).json({
+                success: false,
+                message: "Unauthorized - invalid token"
+            })
+        }
+        req.userId = decoded.userId;
+        next();
+    } catch (error) {
+        console.log("Error in verifyToken", error)
+        res.status(500).json({
+            success: false,
+            message: "Internal server error"
+        })
+
+}};
