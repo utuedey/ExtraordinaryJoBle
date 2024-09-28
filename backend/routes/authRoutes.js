abb02be721852121e642259b1cb339ba0cc976a9
utuedey <utuedeyejosph@gmail.com>
2024-09-28 13:56:04 +0100

    update routes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,16 +1,39 @@
 // backend/routes/auth.js
 const express = require('express');
 const router = express.Router();
-const { authenticateToken } = require('../middlewares/authMiddleware');
-const { register, login, logout } = require('../controllers/authControlller');
+
+const { 
+    signup,
+    login,
+    logout,
+    VerifyEmail,
+    ForgotPassword,
+    ResetPassword,
+    checkAuth
+} = require('../controllers/authController');
+
+const { verifyToken } = require('../middlewares/verifyToken')
+
+
+// Verification token
+router.get('/check-auth', verifyToken, checkAuth);
 
 // Signup route
-router.post('/signup', register)
+router.post('/signup', signup)
 
 // Login route
 router.post('/login', login)
 
 // Logout route
-router.post('/logout', authenticateToken, logout);
+router.post('/logout', logout);
+
+// Email verification link
+router.post('verify-email', VerifyEmail);
+
+// Forget Password route
+router.post('/forgot-password/:token', ForgotPassword);
+
+// Reset Password route
+router.post('/reset-password/:token', ResetPassword);
 
 module.exports = router;
