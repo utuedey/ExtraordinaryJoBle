cce1619ea7f3828df082bfbe7b7acc3523489211
utuedey <utuedeyejosph@gmail.com>
2024-10-14 15:50:36 +0100

    LoginPage Ui

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,7 +1,11 @@
 import {motion} from "framer-motion";
 import Input from '../components/Input';
-import {Mail, User, Lock} from 'lucide-react';
+import {Mail, User, Lock, Loader} from 'lucide-react';
 import { useState } from "react";
+import { Link, useNavigate } from 'react-router-dom';
+import PasswordStrengthMeter from "../components/PasswordStrengthMeter";
+import { useAuthStore } from "../store/authStore";
+
 
 const SignUpPage = () => {
 
@@ -9,9 +13,20 @@ const SignUpPage = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleSignUp = (e) => {
+  const navigate = useNavigate();
+
+  const { signup, error, isLoading} = useAuthStore();
+
+  const handleSignUp = async(e) => {
     e.preventDefault();
-  }
+
+    try {
+      await signup(email, password, name);
+      navigate("/verify-email");
+    } catch (error) {
+      console.log(error)
+    }
+  };
 
   return (
   <motion.div
@@ -47,8 +62,29 @@ const SignUpPage = () => {
     value={password}
     onChange={(e) => setPassword(e.target.value)}
   />
-
-  </form>
+  {error && <p className="text-red-500 font-semibold mt-2">{error}</p>}
+  <PasswordStrengthMeter password={password}/>
+          <motion.button
+          className='mt-5 w-full py-3 px-4 bg-gradient-to-r from-green-500 to-emerald-600 text-white
+          font-bold rounded-lg shadow-lg hover:from-green-600
+          hover:to-emerald-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2
+          focus:ring-offset-gray-900 transition duration-200'
+          whileHover={{ scale: 1.02 }}
+          whileTap={{ scale: 0.98 }}
+          type="submit"
+          disabled={isLoading}
+          >
+          {isLoading ? <Loader className="animate-spin mx-auto" size={24} /> : "Sign Up"}
+          </motion.button>
+        </form>
+      </div>
+      <div className="px-8 py-4 bg-gray-900 bg-opacity-50 flex justify-center">
+      <p className="text-sm text-gray-400">
+        Already have an account?{""}
+        <Link to={"/login"} className="text-green-400 hover:underline">
+        Login
+        </Link>
+      </p>
   </div>
   </motion.div>
 )};
