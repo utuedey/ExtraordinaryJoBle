3c4bb06690d0bdbea3b0d6aecc58250047649ca6
utuedey <utuedeyejosph@gmail.com>
2025-01-04 16:46:03 +0100

    update App,jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import  FloatingShape  from './components/FloatingShape';
 import SignupPage from './components/pages/SignUpPage';
-import LoginPage from './components/pages/loginPage';
+import LoginPage from './components/pages/LoginPage';
 import ForgotPasswordPage from './components/pages/ForgotPasswordPage'
 import EmailVerificationPage from './components/pages/EmailVerificationPage';
 import { Toaster } from 'react-hot-toast';
@@ -35,7 +35,7 @@ const RedirectAuthenticatedUser = ({children}) => {
   const {isAuthenticated, user} = useAuthStore();
 
   if (isAuthenticated && user.isVerified) {
-    return <Navigate to="/" replace />
+    return <Navigate to="/dashboard" replace />
   }
 
   return children
@@ -51,10 +51,10 @@ function App() {
   if (isCheckingAuth) return <LoadingSpinner />
   return (
     <div className='min-h-screen bg-gradient-to-br
-    from-gray-900 via-green-900 to-emerald-900 flex items-center justify-center relative overflow-hidden'>
-    <FloatingShape color='bg-green-500' size='w-64 h-64' top='-5%' left='10%' delay={0} />
-    <FloatingShape color='bg-emerald-500' size='w-48 h-48' top='70%' left='80%' delay={5} />
-    <FloatingShape color='bg-lime-500' size='w-32 h-32' top='40%' left='10%' delay={2}/>
+    from-gray-900 via-white-900 to-blue-400 flex items-center justify-center relative overflow-hidden'>
+    <FloatingShape color='bg-gray-700' size='w-64 h-64' top='-5%' left='10%' delay={0} />
+    <FloatingShape color='bg-blue-500' size='w-48 h-48' top='70%' left='80%' delay={5} />
+    <FloatingShape color='bg-white-500' size='w-32 h-32' top='40%' left='10%' delay={2}/>
 
     <Routes>
     <Route path='/' element={
